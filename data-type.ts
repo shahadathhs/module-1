78471cf0f9e230787f51b12b44fb0e43fc15f5db
shahadathhs <shahadathhossensajib732@gmail.com>
2024-10-30 19:30:01 +0600

    feat: video on data type completed

---
+++ data-type.ts
@@ -0,0 +1,41 @@
+// String
+let firstName: string = "John";
+
+// Number
+let age: number = 10;
+
+// Boolean
+let isActive: boolean = false;
+
+// Null
+let x: null = null;
+
+// Undefined
+let y: undefined = undefined;
+
+// Symbol
+let z: symbol = bd
+
+// any
+let d;
+d = 1
+d = "Sajib"
+d = false
+
+// Array
+const friends: string[] = ["Sajib", "Sharif"]
+const roll: number[] = [1,2,3]
+
+// Tuple -> Array -> order - typer of value
+
+const numbers : [ number, number ] = [1,2]
+
+const ageName: [number, string] = [age, firstName]
+
+// Object
+const object: Object = {
+    firstName,
+    ageName,
+    isActive,
+    x: null
+}
